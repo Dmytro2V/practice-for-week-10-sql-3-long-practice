21db15b713ce51b3c6c80fa5663816da29b5e831
Dmytro2V <op2vin@gmail.com>
2023-03-13 01:39:20 +0200

    done all: init, Adv Phase 1, Phase 2, Phase3, Phase 4

--- sql-practice/phase-4.sql
+++ sql-practice/phase-4.sql
@@ -1,5 +1,14 @@
 -- Give "Red" the cat one of every toy the other cats have
 -- Your code here
 
+INSERT INTO toys (name, cat_id)
+    SELECT DISTINCT name, (SELECT id FROM cats WHERE name = 'Red') FROM toys WHERE cat_id != (SELECT id FROM cats WHERE name = 'Red')
+;
+SELECT * FROM toys ORDER BY cat_id ;
 -- Query spoiled cats reporting the most spoiled first
 -- Your code here
+SELECT 'Cat: ', cats.name, ' toys owned: ', count(toys.name) AS toys_num
+    FROM cats LEFT JOIN toys ON (cats.id = cat_id) 
+    GROUP BY cats.id
+    ORDER BY toys_num DESC
+;
